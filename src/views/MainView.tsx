303ad145e6b0f8fa05cd90a8924dc6ed1a503b0b
shinichi-teshirogi <shinichi-teshirogi@eac-inc.co.jp>
2024-08-18 06:36:01 +0000

    Add Button on MainView

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,8 +1,14 @@
+import React from "react"
+
+const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    alert(`click: ${e.currentTarget.innerHTML}`);
+}
 
 export const MainView = () => {
     return (
         <div>
-            Hello React!
+            <h1>Hello React!</h1>
+            <button onClick={onClick}>Button test</button>
         </div>
     )
 }
