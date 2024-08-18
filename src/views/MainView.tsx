1501bf664d487fe20058c711bfd6071e9ce7b41a
shinichi-teshirogi <shinichi-teshirogi@eac-inc.co.jp>
2024-08-18 07:33:14 +0000

    Add cells and 9 Cell on MainView

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,20 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { Cell } from "../components/Cell";
-
-const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    alert(`click: ${e.currentTarget.innerHTML}`);
-}
-
-const cellClick = (i: number) => {
-    alert("cell clicked")
-}
+import { CellText } from "../types/CellText";
 
 export const MainView = () => {
+    const [isFirst, setIsFirst] = useState(true)
+    const [cells, setCells] = useState<CellText[]>(Array(9).fill(' '))
+    const cellClick = (i: number) => {
+        alert(`cell ${i} clicked`)
+    }
+    
     return (
         <div>
-            <h1>Hello React!</h1>
-            <p><button onClick={onClick}>Button test</button></p>
-            <p><Cell text="X" onClick={() => cellClick(0)} /></p>
+            <h1>Tic-Tac-Toe</h1>
+            <p>
+                <Cell text={cells[0]} onClick={() => cellClick(0)} />
+                <Cell text={cells[1]} onClick={() => cellClick(1)} />
+                <Cell text={cells[2]} onClick={() => cellClick(2)} />
+            </p>
+            <p>
+                <Cell text={cells[3]} onClick={() => cellClick(3)} />
+                <Cell text={cells[4]} onClick={() => cellClick(4)} />
+                <Cell text={cells[5]} onClick={() => cellClick(5)} />
+            </p>
+            <p>
+                <Cell text={cells[6]} onClick={() => cellClick(6)} />
+                <Cell text={cells[7]} onClick={() => cellClick(7)} />
+                <Cell text={cells[8]} onClick={() => cellClick(8)} />
+            </p>
         </div>
     )
 }
